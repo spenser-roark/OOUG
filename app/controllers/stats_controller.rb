0f7810395deeff0190cab5f91e5eb9fcc2ebf159
Spenser Roark <red.arremer.ace@gmail.com>
2014-06-07 22:00:08 -0600

    only admins can see stats now

--- app/controllers/stats_controller.rb
+++ app/controllers/stats_controller.rb
@@ -1,9 +1,17 @@
 class StatsController < ApplicationController
 
+  before_action :correct_user
+
   def index
     @game_count = Games.count(:all)
     @console_count = Consoles.count(:all)
     @accessory_count = Accessories.count(:all)
   end
+
+  private
+
+  def correct_user
+    redirect_to root_path unless admin?
+  end 
 
 end
