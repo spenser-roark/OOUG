9b8d2e0d8a18d933d5fd175da50e8fdee5df54dd
Spenser Roark <red.arremer.ace@gmail.com>
2014-09-11 09:54:57 -0600

    Search by ean

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -4,9 +4,11 @@ class SearchController < ApplicationController
     @search = params[:search]
     wildcard_search = "%#{@search}%"
     
-    @results = Games.where("eng_title like :search or ean like :search", search: wildcard_search).all
-    
-    @count = @results.count;
+    @results1 = Games.where("eng_title like :search or ean like :search", search: wildcard_search).all
+    @results2 = Accessories.where("eng_title like :search or accessories_ean like :search", search: wildcard_search).all
+
+    @count1 = @results1.count
+    @count2 = @results2.count
   end
 
 end
