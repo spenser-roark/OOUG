ae5eb986b1e61edf881a3ecd5dbe2f3d645beb5e
Spenser Roark <red.arremer.ace@gmail.com>
2017-12-30 14:58:08 +0000

    Adding db to gitignore, started a test

--- test/controllers/games_controller_test.rb
+++ test/controllers/games_controller_test.rb
@@ -1,7 +1,11 @@
 require 'test_helper'
 
 class GamesControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+    # test "the truth" do
+    #   assert true
+    # end
+    test "First test I guess" do
+        # gameController = GameController.new
+        assert true
+    end
 end
